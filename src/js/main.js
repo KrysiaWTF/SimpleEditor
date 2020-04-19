49cd69ec8a5b04a3fcd673ad7e124c958bae1c21
KrysiaWTF <j.nickthelittle@gmail.com>
2020-04-19 17:06:27 +0200

    feat: Selection saved to storage function added

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -27,16 +27,67 @@ const buttonLoad = document.querySelector('.button__load--js');
 const buttonSave = document.querySelector('.button__save--js');
 
 
+//figuring out if I can get some part of the text boldened?
 
-var selText = window.getSelection()
+
+//Save selected text in memory
+//var selObj = window.getSelection()
+//var selText = selObj.toString();
+
+// function saveSelection() {
+//   var selection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
+//   localStorage.setItem('Selection', selection);
+// };
+
+// textArea.addEventListener('select', saveSelection)
 
 
-// iconBold.addEventListener('click', () => {
-//   console.log(selText)
-//   selText.classList.add('.form__input--bold');
-// });
+// Save selection
+textArea.addEventListener('select', () => {
+  const selectionObj = {
+    selTarget: event.target,
+    selStart: event.target.selectionStart,
+    selEnd: event.target.selectionEnd,
+    };
+  console.log (selectionObj);
 
-// localStorage.setItem('SavedText', textArea)
+  var selStringify = (JSON.stringify (selectionObj));
+
+  sessionStorage.setItem('selectionObj', selStringify);
+});
+
+
+// get to saved selection
+function parseSelection () {
+  const savedSel = sessionStorage.getItem('selectionObj');
+  console.log (savedSel)
+  if (sessionStorage.getItem('selectionObj') == true) {
+    const selection = JSON.parse(savedSel)
+    console.log(selection);
+  }
+    
+};
+
+
+
+iconItalic.addEventListener('click', parseSelection)
+iconItalic.addEventListener('click', () => {
+  //parseSelection
+  //selText.style.fontStyle = 'italic
+ 
+});
+
+
+
+
+//Bold the text
+iconBold.addEventListener('click', () => {
+  if (textArea.style.fontWeight == 'bold') {
+    textArea.style.fontWeight = 'normal'
+  } else
+    textArea.style.fontWeight = 'bold'
+});
+
 
 
 //Save the text from textarea
